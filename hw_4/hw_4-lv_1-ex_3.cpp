eb98e2c40641e853066c03f05b6d2308742994a4
0xb1b1 <zeroxb1b1@gmail.com>
2021-10-19 02:31:57 +0300

    Fixed formatting in hw_4-lv_1-ex_3

--- hw_4/hw_4-lv_1-ex_3.cpp
+++ hw_4/hw_4-lv_1-ex_3.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main() {
-    int input_matrix[4][4] =  {{1,2,3,4},
-                    {5,6,7,8},
-                    {9,1,2,3},
-                    {4,5,6,7}};
+    int input_matrix[4][4] =   {{1,2,3,4},
+                                {5,6,7,8},
+                                {9,1,2,3},
+                                {4,5,6,7}};
 
     int column_counter = 0, path_sum = 0;
     for(int row = 0; row < 4; row++) {
