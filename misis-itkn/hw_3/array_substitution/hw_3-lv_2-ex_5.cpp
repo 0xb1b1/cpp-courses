50bd3243014fd423ab84799526a540b5a14ad5b5
0xb1b1 <zeroxb1b1@gmail.com>
2021-11-01 04:11:07 +0300

    Fixed hw_3-lv_2-ex_5

--- misis-itkn/hw_3/array_substitution/hw_3-lv_2-ex_5.cpp
+++ misis-itkn/hw_3/array_substitution/hw_3-lv_2-ex_5.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-string arrayToString(double input_array[], int array_size) {
+string arrayToString(int input_array[], int array_size) {
 	string string_output = "";
 	bool first_element = true;
         int passed_elements = 0;
@@ -19,23 +19,25 @@ string arrayToString(double input_array[], int array_size) {
 }
 
 int main() {
-	int n = 10;
-	double input_array[n] = {-880.12,4.23,-3.11,65,-43,111111.11,55,129,21,12};
+	int array_size = 10;
+	int input_array[array_size] = {6,8,-1,5,4,-2,-5,-10,2,5};
 
-	double min_num = input_array[n], max_num = input_array[n];
+	int min_num = input_array[0], max_num = input_array[0];
 	int min_num_index, max_num_index;
-	for(int i = 0; i < n; i++) {
+	for(int i = 0; i < array_size; i++) {
 		if(input_array[i] > max_num) {
 			max_num = input_array[i];
-			max_num_index = i;
-		}  if(input_array[i] < min_num) {
-			min_num = input_array[i];
-			min_num_index = i;
+			max_num_index = i;cout << "max set at " << max_num_index << " num " << input_array[i] << endl;
 		}
-	}
+		if(input_array[i] < min_num) {
+			min_num = input_array[i];
+			min_num_index = i;cout << "min set at " << min_num_index << " num " << input_array[i] << endl;
+		}
+	} cout << "MIN index: " << min_num_index << " MAX index: " << max_num_index << endl;
 
 	cout << min_num << ' ' << max_num;
 
+	// Swap min_num_index and max_num_index if the first one 
 	if(max_num_index > min_num_index) {
 		int temp_index = min_num_index;
 		min_num_index = max_num_index;
@@ -50,7 +52,7 @@ int main() {
         }
 	negative_elements--;
 
-	double negative_array[negative_elements];
+	int negative_array[negative_elements];
         int negative_elements_passed = 0;
 
 	for(int i = max_num_index + 1; i < min_num_index; i++) {
@@ -58,7 +60,7 @@ int main() {
 			negative_array[negative_elements_passed] = input_array[i];
 			negative_elements_passed++;
 		}
-	}
+	} cout << endl;
 
 	cout << arrayToString(negative_array, negative_elements) << endl;
 
