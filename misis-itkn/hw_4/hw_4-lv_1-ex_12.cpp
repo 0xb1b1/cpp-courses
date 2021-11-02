de4e43e3f9089004e62b9039bae24d5adf341245
0xb1b1 <zeroxb1b1@gmail.com>
2021-11-02 21:45:26 +0300

    Added newlines to prevent EOF errors and added comments to hw_4-lv_1-ex_12

--- misis-itkn/hw_4/hw_4-lv_1-ex_12.cpp
+++ misis-itkn/hw_4/hw_4-lv_1-ex_12.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-//#include <string>   // for string int2DMatrixToString
 
 using namespace std;
 
@@ -7,47 +6,39 @@ using namespace std;
 
 string g_debug_arg = "--debug", g_debug_arg_short = "-d";
 
-// it errors out "int (*)[(columns + -1)]"
-/*string int2DMatrixToString(int input_matrix[][7], int rows, int columns) {   // useless as per error "declaration of ‘input_matrix’ as multidimensional array must have bounds for all dimensions except the first", but imma keep it
-    string output_string = "[";
-    for(int row = 0; row < rows; row++) {
-        output_string += "[";
-        for(int column = 0; column < columns; column++) {
-            if(column != columns - 1) { output_string += to_string(input_matrix[row][column]) + ","; }
-            else { output_string += to_string(input_matrix[row][column]); }
-        }
-        if(row != rows - 1) { output_string += "],\n"; }
-        else { output_string += "]]"; }
-    }
-    return(output_string);
-}*/
-
 int main(int argc, char *argv[]) {
+    // Add debug mode
     bool args_present = true, debug_mode = false;
     if(argc == 1) { args_present = false; }
     if(args_present) { if(argv[1] == g_debug_arg || argv[1] == g_debug_arg_short) { debug_mode = true; } }
     if(debug_mode) { cout << "DEBUG MODE ACTIVATED\n"; }
 
+    // Input data
     int rows = 6, columns = 7;
     int input_matrix[rows][columns] =  {{1,3,2,4,1,3,1},
                                         {1,2,4,4,1,4,2},
-                                        {1,2,4,2,6,9,2},    // biggest element here, position 5
+                                        {1,2,4,2,6,9,2},    // The biggest element here, position 5; delete this row
                                         {3,6,4,3,2,4,3},
                                         {2,4,2,5,2,1,1},
                                         {2,4,2,6,3,4,7}};
+                                                // ^ Delete this column
 
-int biggest_num_location[2] = {0,0}, biggest_num = input_matrix[0][0];
-for(int row = 0; row < rows; row++) {
-    for(int column = 0; column < columns; column++) {
-        if(input_matrix[row][column] > biggest_num) {
-            biggest_num = input_matrix[row][column];
-            biggest_num_location[0] = row;
-            biggest_num_location[1] = column;
+    // Find the biggest value in input_matrix and save its coords
+    int biggest_num_location[2] = {0,0}, biggest_num = input_matrix[0][0];
+    for(int row = 0; row < rows; row++) {
+        for(int column = 0; column < columns; column++) {
+            if(input_matrix[row][column] > biggest_num) {
+                biggest_num = input_matrix[row][column];
+                biggest_num_location[0] = row;
+                biggest_num_location[1] = column;
             }
         }
     }
+
+    // Output debug information
     if(debug_mode) { DIVIDER cout << "DEBUG: biggest_num: " << biggest_num << "\nLOCATION: [" << biggest_num_location[0] << "," << biggest_num_location[1] << "]" << endl; DIVIDER }
 
+    // Create the output matrix and put modified data from input_matrix
     int output_matrix[rows - 1][columns - 1];
     int output_matrix_current_row = 0, output_matrix_current_column = 0;
     for(int row = 0; row < rows; row++) {
@@ -61,6 +52,7 @@ for(int row = 0; row < rows; row++) {
         output_matrix_current_column = 0;
     }
 
+    // Output the output matrix
     cout << "[";
     for(int row = 0; row < rows - 1; row++) {
         cout << "[";
@@ -72,8 +64,6 @@ for(int row = 0; row < rows; row++) {
         else { cout << "]"; }
     }
     cout << "]" << endl;
-
-    //cout << int2DMatrixToString(output_matrix, (rows - 1), (columns - 1)) << endl;    // see reason for disabling above int main()
 
     return(0);
 }
