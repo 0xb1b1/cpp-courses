39d12517c493f1efa5375b9792fa20b54d3071bd
0xb1b1 <zeroxb1b1@gmail.com>
2021-11-03 09:09:39 +0300

    Fixed hw_4-lv_1-ex_6

--- misis-itkn/hw_4/hw_4-lv_1-ex_6.cpp
+++ misis-itkn/hw_4/hw_4-lv_1-ex_6.cpp
@@ -1,35 +1,38 @@
 #include <iostream>
-#include <string>   // to create a 1d array, not necessary
 
 using namespace std;
 
 int main() {
+    // Input data
     int rows = 4, columns = 7;
-    int input_matrix[rows][columns] =   {{1,2,3,4,5,6,7},
-                                {8,9,0,1,2,3,4},
-                                {5,6,7,8,9,0,1},
-                                {2,3,4,5,6,7,8}};
-    string min_num_coords[4]; // doesn't work
-    //fill(*input_matrix, *input_matrix + rows*columns, 0); // for later: init an empty matrix
+    int input_matrix[rows][columns] =  {{4,2,3,4,5,6,1},
+                                        {8,9,0,1,2,3,4},
+                                        {5,6,7,8,9,0,1},
+                                        {2,3,4,5,6,7,8}};
     
-    int column_counter = 0, path_sum = 0, min_num_coords_current_member = 0;
+    // Create output array containing coords for the smallest numbers
+    int min_num_coords[4];
+    
+    // Find the smallest number in each row and write it to min_num_coords
     for(int row = 0; row < 4; row++) {
-        int min_num = input_matrix[row][0];
-        string min_num_index = to_string(row) + "," + to_string(0);  // moved for convenience
+        int min_num = input_matrix[row][0],
+            min_num_index = 0;
         for(int column = 1; column < 7; column++) {
             if(input_matrix[row][column] < min_num) {
                 min_num = input_matrix[row][column];
-                min_num_index = to_string(row) + "," + to_string(column);
+                min_num_index = column; cout << "WRITTEN " << column << " TO min_num_index" << endl;
             }
-       }
-       min_num_coords[min_num_coords_current_member] = min_num_index;
-       min_num_coords_current_member++;
+        }
+        min_num_coords[row] = min_num_index; cout << "WRT " << min_num_index << endl;
+        cout << "row: " << row << endl;
     }
 
-    for(int i = 0; i < 4; i++) {
-        cout << min_num_coords[i] << " ";
-    }
-    cout << endl;
+    // Output min_num_coords
+    cout << "[";
+    for(int element = 0; element < rows; element++) {
+        if(element != rows - 1) { cout << min_num_coords[element] << ", ";}
+        else { cout << min_num_coords[element]; }
+    } cout << "]" << endl;
 
     return(0);
 }
