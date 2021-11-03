691dea3e1ffd9b37685bb138fa9d5f6ba1c4f6c8
0xb1b1 <zeroxb1b1@gmail.com>
2021-11-03 04:25:50 +0300

    hw_4-lv_2-ex_9 initial release

---
+++ misis-itkn/hw_4/hw_4-lv_2-ex_9.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // Input data
+    int rows = 6, columns = 7;
+    int input_matrix[rows][columns] =  {{-7,-4, 7, 4,-6, 3,-1},
+                                        { 7,-8, 8, 2,-6,-4, 5},
+                                        { 1, 2, 9, 2, 6, 9,-9},
+                                        {-3, 1, 4,-3,-2,-4, 0},
+                                        { 2, 2, 5, 7, 2, 7, 2},
+                                        { 2,-8, 2,-6, 1, 2,-4}};
+    
+    // Output the input matrix before changing it
+    cout << "Input matrix:\n";
+    cout << "[";
+    for(int row = 0; row < rows; row++) {
+        cout << "[";
+        for(int column = 0; column < columns; column++) {
+            if(input_matrix[row][column] > -1) { cout << " "; }
+            if(column != columns - 1) { cout << input_matrix[row][column] << ","; }
+            else { cout << input_matrix[row][column]; }
+        }
+        if(row != rows - 1) { cout << "],\n "; }
+        else { cout << "]"; }
+    } cout << "]" << endl;
+
+    // Arrange elements of every row in reverse order
+    for(int row = 0; row < rows; row++) {
+        for(int column = 0; column < (columns / 2) % 2 + 2; column++) {
+            int swap_first_element = input_matrix[row][column];
+            input_matrix[row][column] = input_matrix[row][columns - column - 1];
+            input_matrix[row][columns - column - 1] = swap_first_element;
+        }
+    }
+
+    // Output the input matrix
+    cout << "Output matrix:\n";
+    cout << "[";
+    for(int row = 0; row < rows; row++) {
+        cout << "[";
+        for(int column = 0; column < columns; column++) {
+            if(input_matrix[row][column] > -1) { cout << " "; }
+            if(column != columns - 1) { cout << input_matrix[row][column] << ","; }
+            else { cout << input_matrix[row][column]; }
+        }
+        if(row != rows - 1) { cout << "],\n "; }
+        else { cout << "]"; }
+    } cout << "]" << endl;
+
+    return(0);
+}
